5cc2fe433370c11e88f9d69ad155cfb480833fce
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-02-23 15:19:48 +1400

    Refactor - program3 functionality added

--- Linecomparison.java
+++ Linecomparison.java
@@ -1,5 +1,19 @@
-public class Linecomparsioncomputation {
+public class Linecomparison {
     public static void main(String[] args) {
-        System.out.println("\n Welcome to Line Comparsion Computation");
+        
+        double x1 = Math.floor(Math.random() * 10);
+        double x2 = Math.floor(Math.random() * 10);
+        double y1 = Math.floor(Math.random() * 10);
+        double y2 = Math.floor(Math.random() * 10);
+
+        double length1 = Math.floor(Math.sqrt(( x2 - y1 ) * ( x2 - x1 )) + (( y2 - y1 ) * ( y2 - y1 )));
+        double length2 = Math.floor(Math.sqrt(( x2 - y1 ) * ( x2 - x1 )) + (( y2 - y1 ) * ( y2 - y1 )));
+       
+        System.out.println("length for length1 is : " +length1);
+        System.out.println("length for length1 is : " +length1);
+        
+        System.out.println( length1 == length2 ); 
+        System.out.println( length1 > length2 ); 
+        System.out.println( length1 < length2 ); 
     }
 }
